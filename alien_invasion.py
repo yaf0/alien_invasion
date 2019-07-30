627eb473f3a60ef95ecc54afca87b25b6bcbecf3
fan_2 <976864912@qq.com>
2019-07-30 22:40:35 +0800

    create py project alien_invasion

---
+++ alien_invasion.py
@@ -0,0 +1,31 @@
+import sys
+
+import pygame
+
+def run_game():
+    #初始化游戏并建立一个屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen =pygame.display.set_mode((ai_settings.width, ai_settings.height))
+    pygame.display.set_caption("Alien Invasion")
+
+    #创建一艘飞船
+    ship = Ship(screen)
+
+    #设置背景色
+    bg_color = (230, 230, 230)
+
+    #开始游戏的主循环
+    while True:
+
+        #监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+        #让最近绘制的屏幕可见
+        pygame.display.flip()
+
+run_game()
