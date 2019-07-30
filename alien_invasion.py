9fd2ce78eecd660822938e4e7791170434c7d5c6
fan_2 <976864912@qq.com>
2019-07-31 00:44:38 +0800

    实现左右移动

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,7 +19,8 @@ def run_game():
 
     #开始游戏的主循环
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 run_game()
