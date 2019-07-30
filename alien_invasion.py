6865ba8cdf296dd0ff23a53f542ed8750181b80f
fan_2 <976864912@qq.com>
2019-07-30 23:42:01 +0800

    飞船可以正确显示

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,13 @@
 import sys
-
+from settings import Settings
+from ship import Ship
 import pygame
 
 def run_game():
     #初始化游戏并建立一个屏幕对象
     pygame.init()
     ai_settings = Settings()
-    screen =pygame.display.set_mode((ai_settings.width, ai_settings.height))
+    screen =pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     #创建一艘飞船
