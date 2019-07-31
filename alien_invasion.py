cbf1f35171a0203ae8a18ef580b112b1c54023dd
fan_2 <976864912@qq.com>
2019-07-31 19:42:40 +0800

    在能正常左右移动的基础上增加速度调节参数

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,7 +12,7 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     #创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     #设置背景色
     bg_color = (230, 230, 230)
