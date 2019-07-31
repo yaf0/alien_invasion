90431e538592bb4230c63e0cc07ca74ce6980b9c
fan_2 <976864912@qq.com>
2019-07-31 20:47:51 +0800

    增加发射子弹功能

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,25 +2,27 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
+
 import game_functions as gf
 
 def run_game():
-    #初始化游戏并建立一个屏幕对象
+    # 初始化游戏并建立一个屏幕对象
     pygame.init()
     ai_settings = Settings()
     screen =pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #创建一艘飞船
+    # 创建一艘飞船
     ship = Ship(ai_settings, screen)
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
 
-    #设置背景色
-    bg_color = (230, 230, 230)
-
-    #开始游戏的主循环
+    # 开始游戏的主循环
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
